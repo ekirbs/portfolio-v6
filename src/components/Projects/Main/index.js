a26629f4d1b594c47665765fa0aaf003ab048805
Eric K <erickirberger@gmail.com>
2023-08-31 16:10:17 -0400

    responsive styling, spell corrections

--- src/components/Projects/Main/index.js
+++ src/components/Projects/Main/index.js
@@ -6,13 +6,13 @@ export default function Main() {
     <>
       <div className="main">
         <div className="main-section">
-          <h3 className="main-head">Explore Mini Projects</h3>
+          <h3 className="main-head">Explore Desktop Mini Projects</h3>
           <p className="main-text">
             Welcome to my mini projects section! Here, you'll find a curated
             selection of small-scale projects that I've crafted. From
             interactive applications to creative experiments, there's something
             for everyone. Each project offers a unique experience and insight
-            iinto different aspects of web development. Check them out!
+            into different aspects of web development. Check them out!
           </p>
         </div>
       </div>
