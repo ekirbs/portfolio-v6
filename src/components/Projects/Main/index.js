f12ba62b9af377782a96dc22d2eaffba1b32935d
Eric K <erickirberger@gmail.com>
2023-09-04 13:04:24 -0400

    responsive work

--- src/components/Projects/Main/index.js
+++ src/components/Projects/Main/index.js
@@ -8,14 +8,14 @@ export default function Main() {
         <div className="main-section">
           {/* <h3 className="main-head">Explore Mini Projects</h3> */}
           <p className="main-text">
-            Welcome to my mini projects section! Here, you'll find a curated
+            Welcome to my projects section! Here, you'll find a curated
             selection of small-scale projects that I've crafted. From
             interactive applications to creative experiments, there's something
             for everyone. Each project offers a unique experience and insight
-            into different aspects of web development. Check them out!
+            into different aspects of web development. Use the navbar to check them out!
           </p>
-          <p className="main-text">
-            (These projects were designed to be viewed on a full <span className="bold">desktop</span> monitor.  I will be reworking their design to be responsive and mobile-friendly in my spare time.)            
+          <p className="main-text-temp">
+            (These projects were designed to be viewed on a <span className="bold">desktop</span> monitor.  I am reworking their design to be responsive and mobile-friendly in my spare time.)            
           </p>
         </div>
       </div>
